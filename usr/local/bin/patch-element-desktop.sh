61dd9b9aff56263ddb8d291a3a76c467c7d30dc1
Parsa307 <79400634+Parsa307@users.noreply.github.com>
2025-08-30 19:35:08 +0330

    I use CachyOS KDE
    The element patches are not needed for KDE. It was needed in Hyprland, Element had a hardtime trying to find our encryption system.

--- usr/local/bin/patch-element-desktop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DESKTOP_FILE="/usr/share/applications/io.element.Element.desktop"
-
-# Only patch if --password-store is not already present
-if grep -q '^Exec=' "$DESKTOP_FILE" && ! grep -q -- '--password-store="gnome-libsecret"' "$DESKTOP_FILE"; then
-    # Insert gnome-libsecret arg before %u
-    sed -i 's|\(Exec=.*\) %u|\1 --password-store="gnome-libsecret" %u|' "$DESKTOP_FILE"
-fi
